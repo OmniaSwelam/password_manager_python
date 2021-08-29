79838d83d7404eb774788424f3f7a9509b943a2d
Omnia Swelam <omnia.swelam@myocardialsolutions.com>
2021-08-29 11:32:21 +0200

    adding search functionality

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import pyperclip
 from tkinter import *
 from tkinter import messagebox
 from random import randint, choice, shuffle
+import json
+
 MY_EMAIL = "a@b.com"
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -23,6 +26,23 @@ def generate_password():
     password_entry.insert(0, password)
     pyperclip.copy(password)
 
+
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json", mode="r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website_entry.get(), message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} found!")
+
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 
@@ -30,18 +50,34 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please don't leave any of the fields empty!")
     else:
-        is_ok = messagebox.askokcancel(title=website_entry.get(),
-                                       message=f"These are the details entered: \nEmail: {email} \nPassword: {password} "
-                                               f"\nIs it ok to save?")
-        if is_ok:
-            with open("data.txt", mode="a") as file:
-                file.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+        try:
+            with open("data.json", mode="r") as file:
+                # Reading old data
+                data = json.load(file)
+
+        except FileNotFoundError:
+            with open("data.json", mode="w") as file:
+                # Saving new data
+                json.dump(new_data, file, indent=4)
+        else:
+            # Updating old data with new data
+            data.update(new_data)
+            with open("data.json", mode="w") as file:
+                # Saving updated data
+                json.dump(data, file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -65,8 +101,8 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(width=55)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=38)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 email_entry = Entry(width=55)
 email_entry.grid(row=2, column=1, columnspan=2)
@@ -75,6 +111,8 @@ password_entry = Entry(width=38)
 password_entry.grid(row=3, column=1)
 
 # Buttons
+search_button = Button(text="Search", width=14, command=find_password)
+search_button.grid(row=1, column=2)
 generate_password_button = Button(text="Generate Password", width=14, command=generate_password)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=48, command=save)
